263fa8da6e0a5d34fd6969daae1ae96f4b30a00c
Chayan Chandro Sutrodhar <109588297+ChayanSD@users.noreply.github.com>
2022-11-17 16:44:58 +0600

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+ #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout);
+   #endif
+
+    int n; cin>>n;
+
+   int arr[n];
+for(int i=0;i<n;i++){
+cin>>arr[i];
+}
+
+    for(int i = n - 1; i >= 0; i--)
+   {
+        cin >> arr[i];
+        cout << arr[i] << " ";
+    }
+
+	return 0;
+}
