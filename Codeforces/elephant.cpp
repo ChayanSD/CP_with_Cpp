107b13b7515dd554a4c524a241acef6f4661dcaa
Chayan Chandro Sutrodhar <109588297+ChayanSD@users.noreply.github.com>
2022-12-15 23:00:54 +0600

    Add files via upload

---
+++ Codeforces/elephant.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	 #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout);
+   #endif
+     int x;cin>>x;
+     int ans = (x+4)/5;
+     cout<<ans;
+
+	return 0;
+ 
+}
