4cade94b4b45c5fe8ba306e60c73f7e80791cb1d
Chayan Chandro Sutrodhar <109588297+ChayanSD@users.noreply.github.com>
2023-03-16 23:36:18 +0600

    Update basicTemplate.cpp

--- basicTemplate.cpp
+++ basicTemplate.cpp
@@ -1,13 +1,65 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-	 #ifndef ONLINE_JUDGE
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define nextint(x)     scanf("%d",&x)
+#define nextlong(x)     scanf("%lld",&x)
+#define nextstring(s)     scanf("%s",s)
+#define printi(x)     printf("%d\n",x)
+#define printl(x)     printf("%lld\n",x)
+#define prints(s)     printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int>     pii;
+typedef pair<ll, ll>     pl;
+typedef vector<int>          vi;
+typedef vector<ll>          vl;
+typedef vector<pii>          vpii;
+typedef vector<pl>          vpl;
+typedef vector<vi>          vvi;
+typedef vector<vl>          vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+     uniform_int_distribution<int> uid(0,lim-1);
+     return uid(rang);
+}
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;     
+//=======================
+int a[N];
+
+void solve() {
+  int i, j, n, m;
+ 
+}
+int main() {
+//---------------------------------//
+ #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin); 
         freopen("output.txt", "w", stdout);
    #endif
-        cout<<"Hello world!";
 
-	return 0;
- 
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+//---------------------------------//
+    int t = 1;
+    cin >> t;
+    while(t--) {
+      solve();
+    }
+
+    return 0;
 }
+//i am working on it .
+//More things will come soon.
